c3cb81527045e51b6d14066f3a13fb2c8c369460
Fedir <99473719+cyanidium1@users.noreply.github.com>
2025-05-07 15:27:52 +0200

    prices fix

--- src/app/constants/prices.js
+++ src/app/constants/prices.js
@@ -1,14 +1,14 @@
 export const PRICES = {
   BASE: {
-    REGULAR: "20$",
-    SALE: "10$",
+    REGULAR: "30$",
+    SALE: "14$",
   },
   PRO: {
     REGULAR: "40$",
-    SALE: "20$",
+    SALE: "19$",
   },
   EXPERT: {
     REGULAR: "60$",
-    SALE: "30$",
+    SALE: "29$",
   },
 };
